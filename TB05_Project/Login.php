86b71c781eb21e60e689f1ca91f4a4ddf19e9734
tylerglendinning69 <w22005670@northumbria.ac.uk>
2024-11-15 17:04:53 +0000

    removed php files, because php is not possible :(

--- TB05_Project/Login.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'functions.php';
-makeStart('Login');
-?>
-<h1>Login</h1>
-        <a href="VolunteerLogin.html"><button name="VolLogin" type="button">Login as volunteer</button></a>
-        <a href="ManagerLogin.html"><button name="ManLogin" type="button">Login as manager</button></a>
-        <p>New here? Click <a href="register.html">here</a> to create an account</p>
-<?php makeEnd(); ?>
